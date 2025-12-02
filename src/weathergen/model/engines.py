4707d68f972703adcd986ea136a4a1158f821afe
Yura Perugachi Diaz <yperugac@santis-ln001.cscs.ch>
2025-12-02 10:35:44 +0100

    Added LatentState

--- src/weathergen/model/engines.py
+++ src/weathergen/model/engines.py
@@ -6,6 +6,7 @@
 # In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
+import dataclasses
 
 import torch
 import torch.nn as nn
@@ -803,3 +804,14 @@ class TargetPredictionEngine(nn.Module):
             else output
         )
         return output
+
+@dataclasses.dataclass
+class LatentState:
+    """
+    A dataclass to encapsulate the output of latent heads.
+    """
+
+    class_token: torch.Tensor
+    register_tokens: torch.Tensor
+    patch_tokens: torch.Tensor
+    z_pre_norm: torch.Tensor
